a967696c159ba0558731544567179eed3db2cf5d
Lobachev Nikolay <lobachev.nv@yandex.ru>
2021-02-24 13:37:28 +0400

    adding cycle searching

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 rounding_error_limit = 0.0001
 cycle_accept_criteria = 5
+cycle_maximum_size = 10000
 
 
 def get_next_x(f, current_x, r):
@@ -12,44 +13,49 @@ def basic_generator(current_x, r):
 
 def is_it_equals(first_value, second_value):
     value = abs((first_value**2 - second_value**2))**(1/2)
-    return value > 0.0001
+    return value < 0.01
+
+
+def is_equals_arrays(first_array, second_array):
+    if len(first_array) != len(second_array):
+        return False
+
+    for i in range(len(first_array)):
+        if not is_it_equals(first_array[i], second_array[i]):
+            return False
+
+    return True
 
 
 def get_cycle_values(generator, start_x, r):
     current_x = start_x
     cycle_list = [current_x]
 
-    cycle_accepts_count = 0
-    values_accepts_counts = 0
-
-    accuracy_flag = True
-    while cycle_accepts_count < cycle_accept_criteria:
-        cycle_accepts_flag = False
-        while not cycle_accepts_flag:
-            next_x = get_next_x(generator, current_x, r)
-            cycle_accepts_flag = is_it_equals(next_x, cycle_list[values_accepts_counts])
-            if cycle_accepts_flag:
-                values_accepts_counts += 1
-                if values_accepts_counts // len(cycle_list) == 0:
-                    cycle_accepts_count += 1
-                    cycle_accepts_flag = True
-            else:
-                cycle_list.append(next_x)
-                values_accepts_counts = 0
-                cycle_accepts_count = 0
-
-
-        current_x = next_x
-        cycle_list.count()
-    for i in range(20):
+    cycle_accepts_flag = False
+    while not cycle_accepts_flag:
         next_x = get_next_x(generator, current_x, r)
-        accuracy_flag = is_it_equals(current_x, next_x)
-        current_x = next_x
-        print(current_x)
+        print(next_x)
+        cycle_list.append(next_x)
+        for i in range(1, len(cycle_list)//cycle_accept_criteria):
+            cycle = cycle_list[-i:]
+            place_to_cycle_search = cycle_list[-cycle_accept_criteria*i:]
 
-    return current_x
+            cycle_attempt_count = 0
+            for j in range(cycle_accept_criteria):
+                first_cycle_attempt = place_to_cycle_search[:i]
+                place_to_cycle_search = place_to_cycle_search[i:]
+                if is_equals_arrays(cycle, first_cycle_attempt):
+                    cycle_attempt_count += 1
+                    if cycle_attempt_count == cycle_accept_criteria:
+                        return cycle
+
+        if len(cycle_list) > cycle_maximum_size * cycle_accept_criteria:
+            return cycle_list
+
+        current_x = next_x
+
+    return cycle_list
 
 
 if __name__ == '__main__':
-    a = [1, 2, 3, 4, 5, 6, 5, 6]
-    print(a)
+    print(get_cycle_values(basic_generator, 0.4, 3.8))
